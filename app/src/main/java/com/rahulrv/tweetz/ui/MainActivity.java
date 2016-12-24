f79104bd15095c87963199cb196f5db47c0217b2
Rahul RV <rahul@gametime.co>
2016-12-24 15:09:17 -0800

    Setting up checkstyle for style guidelines

--- app/src/main/java/com/rahulrv/tweetz/ui/MainActivity.java
+++ app/src/main/java/com/rahulrv/tweetz/ui/MainActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 
 import com.rahulrv.tweetz.R;
 
+/**
+ * Main launcher activity of the app.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,11 +21,13 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton)
+                findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            public void onClick(final View view) {
+                Snackbar.make(view, "Replace with your own action",
+                        Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
